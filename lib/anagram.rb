4d3f454caf403fbdfc13abfd52ee49d683b057c6
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-10-30 12:48:08 -0400

    commit tree b8e46abfda0bf34f5a951b5f03408d744cde35b2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,36 @@
 # Your code goes here!
+
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+
+        @word = word
+
+    end
+
+    # def split_word
+
+    #     @word.split("").sort #instance variable was split into letters then sorted
+    # end 
+
+   
+    # def split_array(word)
+
+    #     word.split("").sort #argument passed was split into letters then sorted
+    # end 
+
+
+
+    def match(array)
+
+        array.select do |word| #array was iterating and any matches were returned
+            word.split("").sort == @word.split("").sort #argument passed was compared to instance variable
+
+        end 
+
+    end 
+
+end 
